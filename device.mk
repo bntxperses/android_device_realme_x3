50dbc9adba30c8e30296f88722229a138becb28f
Kelvin Zhang <zhangkelvin@google.com>
2025-01-09 13:24:36 +0530

    x3: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false

--- device.mk
+++ device.mk
@@ -495,6 +495,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
+# Update
+AB_OTA_UPDATER := false
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
